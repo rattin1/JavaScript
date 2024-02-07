42f3fa0910fed35f5ba0a0a94cc437c553526341
Ryan Rattini <rattini332@gmail.com>
2024-02-07 14:28:42 -0300

    Exercicio01

---
+++ Exercicio01.js
@@ -0,0 +1,33 @@
+var nome = "Lóide";
+var banco = "Santander";
+var agenc = 40033001;
+var conta = "1732395-4";
+var saldo = 12;
+
+console.log("Dados Da Conta:");
+console.log("");
+console.log("Nome:", nome);
+console.log("Banco:", banco);
+console.log("Número da Agência:", agenc);
+console.log("Saldo Atual:", saldo);
+
+console.log("Transferências:");
+console.log("");
+
+saldo += 29;
+console.log("Pix recebido de Adam Sandler (R$29). Saldo atual:", saldo);
+console.log("___________________________________________________________");
+saldo += 10;
+console.log("Pix recebido de Puro Osso (R$10). Saldo atual:", saldo);
+console.log("___________________________________________________________");
+saldo += 1000;
+console.log("Pix recebido de Felipe Neto (R$1000). Saldo atual:", saldo);
+console.log("___________________________________________________________");
+saldo -= 1050;
+console.log("Pix enviado para Kiko (R$1050). Saldo atual:", saldo);
+console.log("___________________________________________________________");
+saldo += 13000;
+console.log(
+  "Pix recebido de Chavez (R$13000). Saldo atual:",
+  saldo
+);
