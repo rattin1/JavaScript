aec7403a7fb4a73c9fa3a5eb6c2586ca137734a5
Ryan Rattini <rattini332@gmail.com>
2024-02-07 14:30:11 -0300

    tarefa

--- 01-variavel.js
+++ 01-variavel.js
@@ -7,5 +7,13 @@ console.log("Meu nome é", nome, "e tenho", idade, "anos");
 console.log("Agora fiz aniversário! :) Tenho", idade + 1, "anos!");
 
 //aumentando automaticamente +1 (autoincremento)
-idade++ //idade = idade+1
-  console.log("Agora fiz aniversário! :) Tenho", idade, "anos!");
+idade++; //idade = idade+1
+console.log("Agora fiz aniversário! :) Tenho", idade, "anos!");
+
+var v1 = 10;
+var v2 = 3;
+console.log("-----------------------------------");
+console.log("Valores: v1 =", v1, " | v2 =", v2);
+console.log("-----------------------------------");
+console.log("Resto da divisão inteira: v1 % v2 =", v1 % v2);
+console.log("Exponenciação: v1**v2 (v1 elevado à v2) =", v1 ** v2);
