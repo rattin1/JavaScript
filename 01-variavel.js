9886983e3cf3f61d4f15855d066bf5da21cdc197
Ryan Rattini <rattini332@gmail.com>
2024-02-07 11:27:46 -0300

    variavel

---
+++ 01-variavel.js
@@ -0,0 +1,11 @@
+//Criando variáveis com "var" (var nome_variavel = <valor>;)
+
+var nome = "Rattini"; //var cria variavel global
+var idade = 16;
+
+console.log("Meu nome é", nome, "e tenho", idade, "anos");
+console.log("Agora fiz aniversário! :) Tenho", idade + 1, "anos!");
+
+//aumentando automaticamente +1 (autoincremento)
+idade++ //idade = idade+1
+  console.log("Agora fiz aniversário! :) Tenho", idade, "anos!");
---
+++ 02-aritmetico.js
@@ -0,0 +1,30 @@
+//utilizando operadores aritmeticos com JavaScript
+
+console.log("A soma entre 8 e 7 é:", 8 + 7);
+
+//efetuar a soma de dois número informados por você (var nome variavel = 12)
+
+console.log("Quanto é 1+1? É", 1 + 1, "!");
+
+var y = 2;
+var x = 10;
+
+console.log("E dez mais um? É", x + y, "!");
+
+var soma = x + y;
+var subtracao = x - y;
+var multiplicacao = x * y;
+var divisao = x / y;
+
+console.log("Soma = ", soma);
+console.log("Subtracao = ", subtracao);
+console.log("Multiplicacao = ", multiplicacao);
+console.log("Divisao = ", divisao);
+
+//Regras para nomear identificadores (var, const ou let)
+//Podem começar com A-Z, a-z, _ ou $
+
+var MinhaVariavel = 10;
+var outraVariavel = 20;
+const _minhaConstante = 3.1514;
+var $minhaVariavel = 11;
