71ca8a9b72bb0866f820831c0baa23f65f914b7a
Ryan Rattini <rattini332@gmail.com>
2024-02-20 08:53:22 -0300

    20/02

---
+++ 06-variaveisLogicas.js
@@ -0,0 +1,13 @@
+//Variáveis lógicas (boolean)
+
+var nome = "Rattini";
+var rico = false; 
+var eAluno = true;
+
+console.log("Nome:",nome ,"| É aluno?",eAluno,"| É rico?", rico);
+console.log("-----------------------------------------------");
+
+var nome = "Pedro";
+var jogador = false;
+
+console.log("Olá, meu nome é"+ nome +" | É jogador? " + jogador);
---
+++ 07-operadoresRelacionais.js
@@ -0,0 +1,8 @@
+//Operadores Relacionais
+var x = 22;
+var y = 5;
+console.log("--------------------------------------");
+console.log("x =", x, " | y =", y);
+
+console.log("x é igual a y? |", x == y);
+console.log("x é diferente de y? |", x != y);
