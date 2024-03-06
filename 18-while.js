87b91855764129128d98f6a7d67135995063b9b7
Ryan Rattini <rattini332@gmail.com>
2024-03-06 16:29:15 -0300

    06/03

--- 18-while.js
+++ 18-while.js
@@ -1,64 +1,91 @@
-let contador = 0;
+// let contador = 0;
 
-while (contador < 5) {
-  contador++;
-  console.log(contador);
+// while (contador < 5) {
+//   contador++;
+//   console.log(contador);
+// }
+
+// //While testa a condição antes de entrar no laço de repetição
+// //Se a condição for inicialmente falsa, o laço não é executado nenhuma vez.
+// var x = 20;
+
+// while (x > 10) {
+//   console.log("Entrei no laço While...");
+//   x--;
+// }
+// console.log("Terminei!");
+
+// //Contagem regressiva:
+// let countdown = 5;
+
+// while (countdown > 0) {
+//   console.log(countdown);
+//   countdown--;
+// }
+
+// console.log("Lançamento!");
+
+// //Estourando limite do cartão - versão while
+
+// var limite = 500;
+// var fatura = 0;
+
+// while (fatura <= limite) {
+//     let gasto = Math.random() * 100;
+//     fatura += gasto;
+//     console.log(`Gastei R$ ${gasto.toFixed(2)} -------------- Fatura: R$ ${fatura.toFixed(2)}`);
+//     //toFixed(2) alterar o valor mostrado para o usuário em duas casas depois da virgula.
+
+//     fatura > limite
+//     && console.log(`Estourou o limite do cartão`);
+//     //&& faz somente um IF, sem else
+// }
+
+// //Estourando limite do cartão - versão while
+// var limite = 500;
+// var fatura = 0;
+
+// do {
+//     let gasto = Math.random() * 100;
+//     fatura += gasto;
+//     console.log(`Gastei R$ ${gasto.toFixed(2)} -------------- Fatura: R$ ${fatura.toFixed(2)}`
+// );
+
+//     fatura > limite && console.log(`Estourou o limite do cartão`);
+// } while (fatura <= limite);
+
+// //Jogo de cara e coroa - melhor de três tentativas
+// repetição - 3
+// variavel cara ou coroa -> aleatorio - 50%
+// placar - vitorias e derrotas
+
+var rodada = 1;
+var moeda = "";
+var escolhaJogador = "";
+var vitoria = 0;
+var derrota = 0;
+
+while (rodada <= 3) {
+  moeda = Math.random() >= 0.5 ? "Cara" : "Coroa";
+  console.log(moeda);
+  escolhaJogador = Math.random() >= 0.5 ? "Cara" : "Coroa";
+
+  if (escolhaJogador == moeda) {
+    console.log(`Parabéns, você ganhou!`);
+    vitoria++;
+  } else {
+    console.log(`Que pena, você perdeu!`);
+    derrota++
+  }
+  
+  rodada++;
 }
 
-//While testa a condição antes de entrar no laço de repetição
-//Se a condição for inicialmente falsa, o laço não é executado nenhuma vez.
-var x = 20;
-
-while (x > 10) {
-  console.log("Entrei no laço While...");
-  x--;
-}
-console.log("Terminei!");
-
-//Contagem regressiva:
-let countdown = 5;
-
-while (countdown > 0) {
-  console.log(countdown);
-  countdown--;
-}
-
-console.log("Lançamento!");
-
-
-//Estourando limite do cartão - versão while
-
-var limite = 500;
-var fatura = 0;
-
-while (fatura <= limite) {
-    let gasto = Math.random() * 100;
-    fatura += gasto;
-    console.log(`Gastei R$ ${gasto.toFixed(2)} -------------- Fatura: R$ ${fatura.toFixed(2)}`);
-    //toFixed(2) alterar o valor mostrado para o usuário em duas casas depois da virgula.
-
-    fatura > limite 
-    && console.log(`Estourou o limite do cartão`);
-    //&& faz somente um IF, sem else
-} 
-
-//Estourando limite do cartão - versão while
-var limite = 500;
-var fatura = 0;
-
-do {
-    let gasto = Math.random() * 100;
-    fatura += gasto;
-    console.log(`Gastei R$ ${gasto.toFixed(2)} -------------- Fatura: R$ ${fatura.toFixed(2)}`
-);
-
-    fatura > limite && console.log(`Estourou o limite do cartão`);
-} while (fatura <= limite);
-
-
-
-
-
-
-
-
+console.log(`-------------------------`);
+console.log(`         Placar          `);
+console.log(`-------------------------`);
+console.log(`${vitoria>derrota
+   ? " 😂 Você ganhou! 🎰" 
+ : " 🐔 Você perdeu! 🦦"}`
+)
+console.log(`-------------------------`);
---
+++ Exercicio07.js
@@ -0,0 +1,127 @@
+// var popA = 100003;
+// var popB = 300004;
+// var ano = 0;
+
+// while (popA < popB) {
+//   ano++;
+//   popA *= 1.03;
+//   popB *= 1.015;
+//   if (popA >= popB)
+//     console.log(
+//       `Vai demorar ${ano} anos para que a população da cidade A (${Math.round(
+//         popA
+//       )}) seja maior ou igual a da B (${Math.round(popB)})`
+//     );
+// }
+
+
+// var xo = Math.random()
+
+// function jogada() {
+//   if (xo >= 0.5) {
+//     var X = "X"
+//     console.log(X)
+//   }
+//   else
+//   var O = "O" 
+// console.log(O)
+// }
+
+// jogada()
+
+var tabu = [
+  [" ", " ", " "],
+  [" ", " ", " "],
+  [" ", " ", " "],
+];
+
+
+let jogadorX = "X";
+let jogadorO = "O";
+
+function jogar(jogador) {
+  let linha = Math.floor(Math.random() * 3);
+  let coluna = Math.floor(Math.random() * 3);
+  if (tabu[linha][coluna] == " ") tabu[linha][coluna] = jogador;
+  else {
+    jogar(jogador);
+  }
+}
+
+var quemComeca = Math.random()
+
+if (quemComeca >= 0.5) {
+  jogar(jogadorO);
+}
+else jogar(jogadorX)
+
+console.table(tabu);
+// function jogadona() {
+//   while(true) {
+//     const linha = Math.floor(Math.random() * 3);
+//     const coluna = Math.floor(Math.random() * 3);
+//     if (tabu[lnha][coluna] === "") {
+//       return {  linha, coluna };
+//     }
+//   }
+// }
+
+
+// function jogar(linha, coluna, simbolo) {
+//   matriz[linha][coluna] = simbolo;
+// }
+
+// const cleiton = "X";
+
+// while (!verificarVencedor() && !verificarEmpate()) {
+//   if (cleiton === "X") {
+//     const { linha, coluna } = jogadona()
+//     jogar(linha, coluna, cleiton);
+//   }
+//   if (verificarVencedor()) {
+//     console.log(`O jogador ${cleiton} venceu!`);
+//     break;
+//   }
+
+//   if (verificarEmpate()) {
+//     console.log("Empatou!");
+//     break;
+//   }
+
+//   cleiton = cleiton === "X" ? "O" : "X";
+// }
+
+// console.table(tabu);
+
+// function verificarVencedor() {
+//   for(let i = 0; i < tabu.lenght; i++) {
+
+//     if (tabu[i][0] === tabu[i][1] && tabu[i][1] === tabu[i][2] && tabu[i][0] !== "") {
+//       return tabu[i][0];
+//     }
+
+//     if (tabu[0][i] === tabu[1][i] && tabu[1][i] === tabu[2][i] && tabu[0][i] !== "") {
+//       return tabu[0][i];
+//     }
+//   }
+
+//   if (tabu[0][0] === tabu[1][1] && tabu[1][1] === tabu[2][2] && tabu[0][0] !== "") {
+//     return tabu[0][0];
+//   }
+//   if (tabu[0][2] === tabu[1][1] && tabu[1][1] === tabu[2][0] && tabu[0][2] !== "") {
+//     return tabu[0][2];
+//   }
+
+//   return "";
+// }
+
+// console.table(tabu);
+
+console.log("Desculpa, eu tentei.")
+
+
+
+
+
+
+
